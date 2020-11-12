3aa9790878e5ab9641d8a0a03f5b626b64e2a1e8
phher802 <your_email@example.com>
2020-11-12 14:10:40 -0600

    Init

---
+++ RPSLS/Ai.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPSLS
+{
+    class Ai
+    {
+        // member variables (HAS A)
+        Random random;
+
+
+        // constructor (SPAWNER)
+        public Ai()
+        {
+                
+        }
+
+
+        //methods (CAN DO)
+    }
+}
