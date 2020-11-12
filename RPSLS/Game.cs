b7c285282ca10a9d5b6f1a4eee0c6fac01e8aa01
phher802 <your_email@example.com>
2020-11-12 15:36:56 -0600

    Add list of gestures to Game class constructor

--- RPSLS/Game.cs
+++ RPSLS/Game.cs
@@ -8,14 +8,27 @@ namespace RPSLS
 {
     class Game
     {
-
+        // member variables (HAS A)
         Player playerOne;
         Player playerTwo;
-        // member variables (HAS A)
-
-
+        public List<Gestures> gestures;
 
         // constructor (SPAWNER)
+        public Game()
+        {
+            gestures = new List<Gestures>();
+
+            Gestures rock = new Gestures();
+            Gestures paper = new Gestures();
+            Gestures scissor = new Gestures();
+            Gestures lizard = new Gestures();
+            Gestures spock = new Gestures();
+            gestures = new List<Gestures>() { rock, paper, scissor, lizard, spock };
+
+
+
+
+        }
 
 
 
